583313d26065ee7430efcaab47de44d8fd6d084d
Santos1109 <santos1109@unitec.edu>
2017-12-11 22:41:03 -0600

    Actualizacion de TDA Grafo

--- Grafo.cpp
+++ Grafo.cpp
@@ -24,6 +24,49 @@ void Grafo::nuevoElemento(char* elemento)
     }
 }
 
+void GrafoDijkstra::inicializarDijkstra()
+{
+    for(int c = 0; c < TAM; c++)
+    {
+        visitado[c] = false;
+        elementos[c] = 0;
+
+        for(int j = 0; j < TAM; j++)
+        {
+            matriz[c][j] = INT_MAX;
+        }
+    }
+    distancia[0] = matriz[0][0];
+    visitado[0] = true;
+    distancia[0] = 0;
+
+    for(int n = 0; n < TAM; n++)
+    {
+        minimo = INT_MAX;
+        for(int c = 0; c < 10; c++)
+        {
+            if(minimo > distancia[c] && !visitado)
+            {
+                minimo = distancia[c];
+                sig = c;
+            }
+        }
+        visitado[sig] = true;
+
+        for(int s = 0; s < TAM; s++)
+        {
+            if(!visitado[s])
+            {
+                if(minimo+matriz[sig][s] < distancia[s])
+                {
+                    distancia[s] = minimo+matriz[sig][s];
+                    elementos[s] = sig;
+                }
+            }
+        }
+    }
+}
+
 int Grafo::posElemento(char* elemento)
 {
     for(int t = 0; t < TAM; t++)
@@ -131,6 +174,181 @@ bool Grafo::isCompleto()
         }
     }
     return true;
+}
+
+void Grafo::establecerAdyacencias()
+{
+    for(int f = 0; f < tamanio; f++)
+    {
+        for(int c = 0; c < tamanio; c++)
+        {
+            adyacencias[f][c] = false;
+        }
+    }
+}
+
+void Grafo::establecerPesos()
+{
+    for(int f = 0; f < tamanio; f++)
+    {
+        for(int c = 0; c < tamanio; c++)
+        {
+            pesos[f][c] = 0;
+        }
+    }
+}
+
+int Grafo::getPosicion(int num)
+{
+    for(int s = 0; s < tamanio; s++)
+    {
+        if(numeros[s] == num)
+        {
+            return s;
+        }
+    }
+    return -1;
+}
+
+bool Grafo::crearAdyacencia(int num1, int num2)
+{
+    if(!adyacencias[getPosicion(num1)][getPosicion(num2)]
+       || !adyacencias[getPosicion(num2)][getPosicion(num1)])
+    {
+        adyacencias[getPosicion(num1)][getPosicion(num2)] = true;
+        adyacencias[getPosicion(num2)][getPosicion(num1)] = true;
+        return true;
+    }
+    return false;
+}
+
+void Grafo::eliminarAdyacencia(int num1, int num2)
+{
+    adyacencias[getPosicion(num1)][getPosicion(num2)] = false;
+    adyacencias[getPosicion(num2)][getPosicion(num1)] = false;
+}
+
+bool Grafo::existeCamino(int num1, int num2)
+{
+    bool verticesVisitados[10] = {0,0,0,0,0,0,0,0,0,0};
+    verticesVisitados[getPosicion(num1)] = true;
+    stack<int> siguiente;
+    siguiente.push(getPosicion(num1));
+
+    while(!siguiente.empty())
+    {
+        int temp = siguiente.top();
+        siguiente.pop();
+
+        for(int s = 0; s < tamanio; s++)
+        {
+            if(!verticesVisitados[s] && adyacencias[temp][s])
+            {
+                verticesVisitados[s] = true;
+                siguiente.push(s);
+            }
+        }
+    }
+    return verticesVisitados[getPosicion(num2)];
+}
+
+void Grafo::agregarPesos(int num1, int num2, int peso)
+{
+    pesos[getPosicion(num1)][getPosicion(num2)] = peso;
+    pesos[getPosicion(num2)][getPosicion(num1)] = peso;
+}
+
+int Grafo::caminoMinimo(int num1, int num2)
+{
+    int temporal[tamanio][tamanio];
+
+    for(int s = 0; s < tamanio; s++)
+    {
+        for(int x = 0; x < tamanio; x++)
+        {
+            temporal[s][x] = pesos[s][x];
+        }
+    }
+
+    for(int n = 0; n < tamanio; n++)
+    {
+        for(int m = 0; m < tamanio; m++)
+        {
+            for(int s = 0; s < tamanio; s++)
+            {
+                if(temporal[m][n]+temporal[n][s] < temporal[m][s])
+                {
+                    temporal[m][s] = temporal[m][n] + temporal[n][s];
+                }
+            }
+        }
+    }
+    return temporal[getPosicion(num1)][getPosicion(num2)];
+}
+
+bool Grafo::isConexo()
+{
+    int puntos = 0;
+
+    for(int f = 0; f < tamanio; f++)
+    {
+        for(int c = 0; c < tamanio; c++)
+        {
+            if(adyacencias[f][c])
+            {
+                puntos++;
+            }
+        }
+        if(puntos >= 1)
+        {
+            puntos = 0;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Grafo::isCompleto()
+{
+    for(int f = 0; f < tamanio; f++)
+    {
+        for(int c = 0; c < tamanio; c++)
+        {
+            if(!adyacencias[f][c])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Grafo::imprimirAdyacencias()
+{
+    for(int f = 0; f < tamanio; f++)
+    {
+        for(int c = 0; c < tamanio; c++)
+        {
+            cout<<adyacencias[f][c]<<" ";
+        }
+        cout<<endl<<endl;
+    }
+}
+
+
+
+void Grafo::imprimirPesos()
+{
+    for(int f = 0; f < tamanio; f++)
+    {
+        for(int c = 0; c < tamanio; c++)
+        {
+            cout<<pesos[f][c]<<" ";
+        }
+        cout<<endl<<endl;
+    }
 }
 
 Grafo::~Grafo()
--- Grafo.h
+++ Grafo.h
@@ -24,6 +24,36 @@ class Grafo
         bool isCompleto();
         virtual ~Grafo();
 
+        static const int TAM = 10;
+        int minimo = INT_MAX, sig = 0;
+        int elementos[TAM];
+        int matriz[TAM][TAM];
+        int distancia[TAM];
+        bool visitado[TAM];
+        GrafoDijkstra();
+        void inicializarDijkstra();
+        void dijkstra(int inicio);
+        virtual ~GrafoDijkstra();
+
+        static const int tamanio = 10;
+        int numeros[10] = {20,33,54,22,90,67,78,96,12,48};
+        bool adyacencias[tamanio][tamanio];
+        int pesos[tamanio][tamanio];
+        Grafo();
+        void establecerAdyacencias();
+        void establecerPesos();
+        bool crearAdyacencia(int num1, int num2);
+        void agregarPesos(int num1, int num2, int peso);
+        void eliminarAdyacencia(int num1, int num2);
+        bool existeCamino(int num1, int num2);
+        int caminoMinimo(int num1, int num2);
+        bool isConexo();
+        bool isCompleto();
+        int getPosicion(int num);
+        void imprimirAdyacencias();
+        void imprimirPesos();
+
+
     protected:
 
     private:
